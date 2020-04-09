fcee8aa81d7ba009a679c10550b82c6d1db4f3d7
m1cro1ce <shams.s@foundationai.com>
2020-04-09 03:26:30 -0400

    ADD: changes in the run-docker file

--- docker-dl-setup/run-docker.sh
+++ docker-dl-setup/run-docker.sh
@@ -1,1 +1,17 @@
-docker run --rm -d --name kratos_devel --gpus all --ipc=host -p 8888:8888 -p 6006:6006 -v /home/m1cro1ce/WorkSpace:/WorkSpace dev-py36:latest jupyter notebook --ip=0.0.0.0 --allow-root --no-browser
+name=zeus_devel
+
+docker run --rm \
+       -d \
+       --name $name \
+       --hostname $name \
+       --gpus all \
+       --ipc=host \
+       -p 8888:8888 \
+       -p 6006:6006 \
+       -v /home/m1cro1ce/WorkSpace:/WorkSpace \
+       dev-py36:latest \
+       jupyter notebook \
+       --ip=0.0.0.0 \
+       --allow-root \
+       --no-browser
+
