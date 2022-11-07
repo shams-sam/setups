abacea2c753995de4b56019abfe9eb6d1cd10edc
shams-sam <s.shams.sam@gmail.com>
2022-11-07 23:04:05 +0000

    FIX: escape special characters within the string

--- init_terminal.sh
+++ init_terminal.sh
@@ -30,12 +30,14 @@ source /home/$USER/miniconda3/etc/profile.d/conda.sh
 conda create --yes --name py37 python=3.7
 echo "source /home/$USER/miniconda3/etc/profile.d/conda.sh
 conda activate py37
+
 # reference:
 # https://askubuntu.com/a/730758
 parse_git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-if [ "$color_prompt" = yes ]; then
+color_prompt=yes
+if [ \"$color_prompt\" = yes ]; then
  PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
 else
  PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
