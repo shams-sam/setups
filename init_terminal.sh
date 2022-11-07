4ccf4f0472aa244c30b41bdc7e63ec204b76c63e
Shams Azam <s_azam@s-azam-task.siri-interactive-vm.svc.kube.us-west-3b.k8s.cloud.apple.com>
2022-11-07 22:55:21 +0000

    ADD: changes in init_terminal script

--- init_terminal.sh
+++ init_terminal.sh
@@ -1,7 +1,7 @@
-cd ~
-mkdir workspace
-cd 
-git clone https://github.com/shams-sam/setups.git
+# cd ~
+# mkdir workspace
+# cd workspace
+# git clone https://github.com/shams-sam/setups.git
 
 sudo add-apt-repository -y ppa:kelleyk/emacs \
      main \
@@ -11,7 +11,7 @@ sudo add-apt-repository -y ppa:kelleyk/emacs \
 
 # required to install python-ldap required by atlas
 # https://stackoverflow.com/questions/4768446/i-cant-install-python-ldap
-sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
+sudo apt-get install -y libsasl2-dev python-dev libldap2-dev libssl-dev
 
 sudo apt update -y
 
@@ -24,15 +24,26 @@ sudo apt install -y \
 # miniconda installation
 # https://towardsdatascience.com/how-to-install-miniconda-x86-64-apple-m1-side-by-side-on-mac-book-m1-a476936bfaf0
 # https://docs.conda.io/en/latest/miniconda.html#linux-installers
-wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh
-sh ./Miniconda3-py37_4.12.0-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh ./Miniconda3-latest-Linux-x86_64.sh -b
 source /home/$USER/miniconda3/etc/profile.d/conda.sh
 conda create --yes --name py37 python=3.7
-echo $'source /home/$USER/miniconda3/etc/profile.d/conda.sh\nconda activate py37\nalias ll="ls -al"' >> ~/.profile
+echo "source /home/$USER/miniconda3/etc/profile.d/conda.sh
+conda activate py37
+# reference:
+# https://askubuntu.com/a/730758
+parse_git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+if [ "$color_prompt" = yes ]; then
+ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+else
+ PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
+fi" >> ~/.profile
 source ~/.profile
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp setups/.tmux.conf.maglev ~/.tmux.conf
+cp .tmux.conf.maglev ~/.tmux.conf
 
 mkdir ~/.emacs.d
-cp setups/emacs.init.el ~/.emacs.d/init.el
+cp emacs.init.el ~/.emacs.d/init.el
